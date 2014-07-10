bdaca84c103acad96395dd0eafb7e3cc3dd30ec3
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-07-10 11:24:29 -0400

    devise

--- v0/app/controllers/application_controller.rb
+++ v0/app/controllers/application_controller.rb
@@ -11,5 +11,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:accept_invitation) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
   end
 end
