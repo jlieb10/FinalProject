f3dd02ba86d2220b7fa5cea83b1208c8fc433c01
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-07-14 20:37:45 -0400

    Deleted Image Files

--- v0/config/environments/development.rb
+++ v0/config/environments/development.rb
@@ -32,7 +32,18 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings {
+  #   :address => "smtp.gmail.com",
+  #   :port => "587",
+  #   :domain => "gmail.com",
+  #   :authentication => "plain",
+  #   :enable_starttls_auto => true,
+  #   :user_name => ENV["EMAIL_KEY"],
+  #   :password => ENV["EMAIL_SECRET"]
+  # }
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.delivery_method = :letter_opener
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
