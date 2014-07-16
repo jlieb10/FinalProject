be7e2060bd80ed028bf420337e8f5d649900f363
stephaniecoleman <sproutwingsandfly@gmail.com>
2014-07-16 16:59:07 -0400

    Set default select value for status dropdown.

--- V0.1a/app/assets/javascripts/application.js
+++ V0.1a/app/assets/javascripts/application.js
@@ -16,4 +16,46 @@
 //= require ui.js
 //= require app.js
 //= require_tree .
-//= require apartments.js
+
+$(document).ready(function(){
+  $('input.con, input.pro').change(function(){
+    var form = $(this.form);
+
+    var data = {
+      authenticity_token: form.find("input[name='authenticity_token']").val(),
+      detail: {procon: $(this).val()}
+    };
+
+    $.ajax({
+      type: 'Put',
+      url: form.attr('action'),
+      data: data,
+      dataType: 'json'
+    }).success(function(data){
+      $('li[data-id="' + data.id + '"]').remove();
+      var listItem = "<li data-id='"+ data.id + "'>" + data.content + "</li>"
+      if (data.procon === "pro") {
+        $("ul.pro").append(listItem);
+      }
+      else {
+        $("ul.con").append(listItem);
+      }
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#apartment_status').change(function(){
+    $.ajax({
+      type: 'Put',
+      url: $(this).parent().siblings().children().attr("href"),
+      data: {"authenticity_token" : $("meta").last().attr("content"), 
+              "apartment" : {"status" : $("#apartment_status option:selected").text()}
+            },
+      dataType: 'json',
+      success: function(data) {
+        
+      }
+    })
+  })
+})
