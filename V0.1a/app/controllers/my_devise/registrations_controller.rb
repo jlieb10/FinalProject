95f978e56fdd26181fa54f80c69265a0e9e0499f
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-07-18 11:24:52 -0400

    Updated Profile

--- V0.1a/app/controllers/my_devise/registrations_controller.rb
+++ V0.1a/app/controllers/my_devise/registrations_controller.rb
@@ -22,6 +22,19 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
   end
 
   def update
+    user = User.find_by_id(params[:id])
+    unless user.blank?
+      params[:user].delete(:password) if params[:user][:password].blank?
+      params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+      if user.update_attributes(params[:user])
+        flash[:notice] = "User updated successfully."
+      else
+        render :action => 'edit'
+      end
+    else
+      render :action => 'edit'
+    end
+
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
@@ -34,10 +47,10 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
         set_flash_message :notice, flash_key
       end
       sign_in resource_name, resource, bypass: true
-      respond_with resource, location: after_update_path_for(resource)
+      #respond_with resource, location: after_update_path_for(resource)
     else
       clean_up_passwords resource
-      respond_with resource
+      #respond_with resource
     end
   end
 
