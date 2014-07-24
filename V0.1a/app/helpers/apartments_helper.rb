e200b31462c6297a393ddabda8c99819cbabdd32
Joshua T. Lieberman <joshuathomaslieberman@gmail.com>
2014-07-24 11:24:58 -0400

    Fixed Apartment Detail Show

--- V0.1a/app/helpers/apartments_helper.rb
+++ V0.1a/app/helpers/apartments_helper.rb
@@ -6,4 +6,20 @@ module ApartmentsHelper
 			@apartment
 		end
 	end
+
+  def render_details detail
+    locals = {
+      detail: detail,
+    }
+
+    case detail.procon
+    when 'pro'
+      locals[:klass] = 'pro-label'
+    when 'con'
+      locals[:klass] = 'con-label'
+    else
+      locals[:klass] = ''
+    end
+    render 'apartments/detail', locals
+  end
 end
